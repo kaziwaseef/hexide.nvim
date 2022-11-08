813847637bfd76ed75f9f416cf882e0d1a118af1
Kazi Waseef <kaziwaseef@gmail.com>
2022-11-08 13:05:38 +0600

    feat: vgit keymap

--- lua/rawnvim/pluginConfig/vgit.lua
+++ lua/rawnvim/pluginConfig/vgit.lua
@@ -1,1 +1,9 @@
 require("vgit").setup()
+
+local opts = { noremap = true, silent = true }
+
+local keymap = vim.api.nvim_set_keymap
+
+local map = require("rawnvim.osKeyMap").mapping
+
+keymap("n", "<leader>gh", ":VGit buffer_history_preview<CR>", opts)
