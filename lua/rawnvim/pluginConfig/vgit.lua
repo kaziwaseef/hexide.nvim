259f86dd8f27ea110cc63a257daeb264d4eccd23
Kazi Waseef <kaziwaseef@gmail.com>
2022-11-08 13:06:41 +0600

    refactor: removed unused import

--- lua/rawnvim/pluginConfig/vgit.lua
+++ lua/rawnvim/pluginConfig/vgit.lua
@@ -4,6 +4,4 @@ local opts = { noremap = true, silent = true }
 
 local keymap = vim.api.nvim_set_keymap
 
-local map = require("rawnvim.osKeyMap").mapping
-
 keymap("n", "<leader>gh", ":VGit buffer_history_preview<CR>", opts)
