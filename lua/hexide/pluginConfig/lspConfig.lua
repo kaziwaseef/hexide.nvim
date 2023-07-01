941e14915dfe5a49a1088c6ee1acdc4bbd198063
Kazi Waseef <kaziwaseef@gmail.com>
2023-07-01 16:39:05 +0600

    fix: fidget fixed

--- lua/hexide/pluginConfig/lspConfig.lua
+++ lua/hexide/pluginConfig/lspConfig.lua
@@ -1,12 +1,18 @@
-local map = require("hexide.osKeyMap").mapping
-
 local M = {
 	"williamboman/mason.nvim",
 	"WhoIsSethDaniel/mason-tool-installer.nvim",
 	"williamboman/mason-lspconfig.nvim",
 	"neovim/nvim-lspconfig",
 	"akinsho/flutter-tools.nvim",
-	{ "j-hui/fidget.nvim", lazy = false },
+	{
+		"j-hui/fidget.nvim",
+		event = "BufEnter",
+		config = function()
+			require("fidget").setup()
+		end,
+		tag = "legacy",
+		dependencies = { "neovim/nvim-lspconfig" },
+	},
 	{
 		"jose-elias-alvarez/null-ls.nvim",
 		config = function()
