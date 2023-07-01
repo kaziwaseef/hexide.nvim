498f3e8a07cca32be2534a76997d21e9ef674155
Kazi Waseef <kaziwaseef@gmail.com>
2023-07-01 18:16:47 +0600

    feat: legendary updated key from map

--- lua/hexide/pluginConfig/legendaryPlugins.lua
+++ lua/hexide/pluginConfig/legendaryPlugins.lua
@@ -1,12 +1,16 @@
+local map = require("hexide.osKeyMap").mapping
+
 local M = {
 	{
 		"mrjones2014/legendary.nvim",
 		priority = 10000,
 		lazy = false,
 		dependencies = { "kkharji/sqlite.lua" },
-		keys = { { "†", "<cmd>Legendary<cr>", desc = "Open Command Palette" } },
+		keys = { { map.CMD_SHIFT_T, "<cmd>Legendary<cr>", desc = "Open Command Palette" } },
 		config = function()
-			require("legendary").setup({ lazy_nvim = { auto_register = true } })
+			require("legendary").setup({
+				lazy_nvim = { auto_register = true },
+			})
 		end,
 	},
 }
