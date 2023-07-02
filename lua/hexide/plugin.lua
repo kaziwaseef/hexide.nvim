56708e5dd2f893a95b1c60f550fd2f6db424abd6
Kazi Waseef <kaziwaseef@gmail.com>
2023-07-02 10:48:42 +0600

    feat: updated colorscheme

--- lua/hexide/plugin.lua
+++ lua/hexide/plugin.lua
@@ -40,9 +40,15 @@ local commonPlugins = {
 
 local colorSchemes = {
 	{
-		"joshdick/onedark.vim",
+		"navarasu/onedark.nvim",
 		lazy = false, -- make sure we load this during startup if it is your main colorscheme
 		priority = 1000, -- make sure to load this before all the other start plugins
+		config = function()
+			require("onedark").setup({
+				style = "warm",
+			})
+			require("onedark").load()
+		end,
 	},
 }
 
