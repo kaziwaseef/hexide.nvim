71d182815927f39e9c2b01204a4c8b0f92825746
Kazi Waseef <kaziwaseef@gmail.com>
2025-10-11 02:34:12 +0600

    feat: Add neotree open file with system

--- lua/hexide/pluginConfig/neoTree.lua
+++ lua/hexide/pluginConfig/neoTree.lua
@@ -8,20 +8,24 @@ local M = {
 		keys = {
 			{
 				map.CMD_t,
-				"<cmd>Neotree source=filesystem reveal=true position=right<cr>",
+				"<cmd>Neotree source=filesystem reveal=true<cr>",
 				desc = "Neo Tree File Browser",
 			},
 		},
-		branch = "v2.x",
+		branch = "v3.x",
 		dependencies = {
 			"nvim-lua/plenary.nvim",
 			"MunifTanjim/nui.nvim",
+			"nvim-tree/nvim-web-devicons",
+			"folke/snacks.nvim",
+			"antosha417/nvim-lsp-file-operations",
 		},
 		config = function()
 			require("neo-tree").setup({
 				close_if_last_window = true,
 				window = {
-					position = "right",
+					position = "left",
+					width = 30,
 				},
 				filesystem = {
 					use_libuv_file_watcher = true,
@@ -34,6 +38,23 @@ local M = {
 							"thumbs.db",
 						},
 					},
+					window = {
+						mappings = {
+							["o"] = "system_open",
+						},
+					},
+				},
+				commands = {
+					system_open = function(state)
+						local node = state.tree:get_node()
+						local path = node:get_id()
+
+						if vim.loop.os_uname().sysname == "Darwin" then
+							vim.fn.jobstart({ "open", path }, { detach = true })
+						else
+							vim.fn.jobstart({ "xdg-open", path }, { detach = true })
+						end
+					end,
 				},
 				default_component_configs = {
 					git_status = {
