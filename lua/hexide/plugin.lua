f0443555257575a4dfc88e627861fd4fbb1a0587
Kazi Waseef <kaziwaseef@gmail.com>
2025-10-11 02:33:54 +0600

    feat: Remove tabs

--- lua/hexide/plugin.lua
+++ lua/hexide/plugin.lua
@@ -5,7 +5,7 @@ local completionPlugins = require("hexide.pluginConfig.completion")
 local telescopePlugins = require("hexide.pluginConfig.telescope")
 local treesitterPlugins = require("hexide.pluginConfig.treesitter")
 local gitPlugins = require("hexide.pluginConfig.git")
-local tabPlugins = require("hexide.pluginConfig.barbar")
+-- local tabPlugins = require("hexide.pluginConfig.barbar")
 local neoTreePlugins = require("hexide.pluginConfig.neoTree")
 local lspConfigPlugins = require("hexide.pluginConfig.lspConfig")
 local legendaryPlugins = require("hexide.pluginConfig.legendaryPlugins")
@@ -64,11 +64,38 @@ local airLinePlugins = {
 	},
 }
 
+local aiPlugins = {
+	{
+		"coder/claudecode.nvim",
+		dependencies = { "folke/snacks.nvim" },
+		config = true,
+		keys = {
+			{ "<leader>a", nil, desc = "AI/Claude Code" },
+			{ "<leader>ac", "<cmd>ClaudeCode<cr>", desc = "Toggle Claude" },
+			{ "<leader>af", "<cmd>ClaudeCodeFocus<cr>", desc = "Focus Claude" },
+			{ "<leader>ar", "<cmd>ClaudeCode --resume<cr>", desc = "Resume Claude" },
+			{ "<leader>aC", "<cmd>ClaudeCode --continue<cr>", desc = "Continue Claude" },
+			{ "<leader>am", "<cmd>ClaudeCodeSelectModel<cr>", desc = "Select Claude model" },
+			{ "<leader>ab", "<cmd>ClaudeCodeAdd %<cr>", desc = "Add current buffer" },
+			{ "<leader>as", "<cmd>ClaudeCodeSend<cr>", mode = "v", desc = "Send to Claude" },
+			{
+				"<leader>as",
+				"<cmd>ClaudeCodeTreeAdd<cr>",
+				desc = "Add file",
+				ft = { "NvimTree", "neo-tree", "oil", "minifiles", "netrw" },
+			},
+			-- Diff management
+			{ "<leader>aa", "<cmd>ClaudeCodeDiffAccept<cr>", desc = "Accept diff" },
+			{ "<leader>ad", "<cmd>ClaudeCodeDiffDeny<cr>", desc = "Deny diff" },
+		},
+	},
+}
+
 local internalPlugins = tableUtils.spreadTables(
 	commonPlugins,
 	legendaryPlugins,
 	colorSchemes,
-	tabPlugins,
+	-- tabPlugins,
 	treesitterPlugins,
 	lspConfigPlugins,
 	airLinePlugins,
@@ -77,7 +104,8 @@ local internalPlugins = tableUtils.spreadTables(
 	neoTreePlugins,
 	gitPlugins,
 	motionPlugins,
-	searchPlugins
+	searchPlugins,
+	aiPlugins
 )
 
 local plugins = internalPlugins
