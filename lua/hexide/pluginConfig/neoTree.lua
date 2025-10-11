2964d2461939402a175b8a5f32e578a56a582c0b
Kazi Waseef <kaziwaseef@gmail.com>
2025-10-11 07:13:43 +0600

    feat: Add neotree copy

--- lua/hexide/pluginConfig/neoTree.lua
+++ lua/hexide/pluginConfig/neoTree.lua
@@ -41,6 +41,40 @@ local M = {
 					window = {
 						mappings = {
 							["o"] = "system_open",
+							["y"] = "copy_relative_path",
+							["Y"] = "copy_absolute_path",
+						},
+					},
+					components = {
+						harpoon_index = function(config, node, _)
+							local harpoon_list = require("harpoon"):list()
+							local path = node:get_id()
+							local harpoon_key = vim.uv.cwd()
+
+							for i, item in ipairs(harpoon_list.items) do
+								local value = item.value
+								if string.sub(item.value, 1, 1) ~= "/" then
+									value = harpoon_key .. "/" .. item.value
+								end
+
+								if value == path then
+									vim.print(path)
+									return {
+										text = string.format(" ⥤ %d", i),
+										highlight = config.highlight or "NeoTreeDirectoryIcon",
+									}
+								end
+							end
+							return {}
+						end,
+					},
+					renderers = {
+						file = {
+							{ "icon" },
+							{ "name", use_git_status_colors = true },
+							{ "harpoon_index" }, --> This is what actually adds the component in where you want it
+							{ "diagnostics" },
+							{ "git_status", highlight = "NeoTreeDimText" },
 						},
 					},
 				},
@@ -54,6 +88,18 @@ local M = {
 						else
 							vim.fn.jobstart({ "xdg-open", path }, { detach = true })
 						end
+					end,
+					copy_absolute_path = function(state)
+						local node = state.tree:get_node()
+						local path = node:get_id()
+						vim.fn.setreg("+", path)
+						vim.notify("Copied: " .. path)
+					end,
+					copy_relative_path = function(state)
+						local node = state.tree:get_node()
+						local rel_path = vim.fn.fnamemodify(node:get_id(), ":.")
+						vim.fn.setreg("+", rel_path)
+						vim.notify("Copied: " .. rel_path)
 					end,
 				},
 				default_component_configs = {
--- lua/hexide/todo.md
+++ lua/hexide/todo.md
@@ -4,3 +4,5 @@
 - [ ] Update to use asdf-managed LSPs
   - [x] Update lspConfig.lua to use asdf-managed LSPs
   - [ ] Add LSPs to asdf plugins automatically
+- [x] Add copy path to neotree
+- [ ] Make sure ai edits show up better: [Nabeel's Code](https://github.com/exaby73/kickstart.nvim/blob/4fccc66d79ecbe043e0904c4000f9bd60efc4ac0/lua/core/autocommands.lua)
